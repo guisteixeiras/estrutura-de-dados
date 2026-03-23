d9d36fe588112d0e7cef3c2e908f411099d48a84
Guilherme <guiteixeira191@icloud.com>
2026-03-23 16:33:38 -0300

    Atividades da aula 8

---
+++ aula_8/atv_8.c
@@ -0,0 +1,16 @@
+/*Crie um programa em C que leia uma matriz quadrada N × N de números inteiros, onde N é informado pelo usuário (máx. 10).
+O programa deve:
+
+Calcular a soma da diagonal principal
+Calcular a soma da diagonal secundária
+Contar quantos elementos estão acima da média da matriz
+Verificar se a matriz é simétrica
+Exibir os resultados obtidos*/
+
+
+
+int main () {
+
+
+
+}
