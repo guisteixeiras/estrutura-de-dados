d200378ac33c41f64ba6f085fc85f8f991106199
guisteixeiras <guiteixeira191@icloud.com>
2026-04-03 15:59:28 -0300

    Atividade da aula 8 que estava incompleta finalizada

--- aula_5/atv3_complexidadeO2n.c
+++ aula_5/atv3_complexidadeO2n.c
@@ -1,1 +1,30 @@
-// Não consegui fazer a atv 3
+#include <stdio.h>
+
+
+int fibonacci(int num) { //função
+
+    if (num == 0) {
+        return 0; // caso base
+    } else if (num == 1) { // caso base
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2); //recirsividade
+    }
+}
+
+    int main () {
+
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("O número não pode ser negativo!");
+    } else {
+        printf("Resultado: %d", fibonacci(num));
+    } 
+
+    return 0;
+
+}
